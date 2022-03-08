656413109ccb9388f5a202ce70a618ec70675ba8
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2022-03-08 14:38:59 +0530

    coded

---
+++ src/main/java/com/java/boot/ProductCrudOperationSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.java.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCrudOperationSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCrudOperationSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/boot/ProductCrudOperationSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCrudOperationSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
